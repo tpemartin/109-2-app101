aff6425b4726da44ed7934093630fc853669b2f9
tpemartin <tpemartin@gmail.com>
2021-04-02 19:20:58 +0800

    update UI

--- docs/lib/myown-1.0/js/mydesign.js
+++ docs/lib/myown-1.0/js/mydesign.js
@@ -3,5 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var instance_tabs = M.Tabs.init(el_tabs);
     var el_sidenav = document.querySelectorAll('.sidenav');
     var instance_sidenav = M.Sidenav.init(el_sidenav);
-    instance_sidenav.open();
+    /* instance_sidenav.open(); */
+    var el_fixActBtn = document.querySelectorAll('.fixed-action-btn');
+    var instance_fixActBtn = M.FloatingActionButton.init(el_fixActBtn);
 });
