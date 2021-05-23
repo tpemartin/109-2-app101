952bbda1bec798d4fd950fe04dc02508c6d303a7
Simmonslin <71751182+Simmonslin@users.noreply.github.com>
2021-05-23 15:52:41 +0800

    Add files via upload

--- R/chinese_martin.R
+++ R/chinese_martin.R
@@ -1,4 +1,14 @@
-chinese <- list()
+---
+title: "Untitled"
+output: html_document
+---
+
+
+
+
+
+
+  chinese <- list()
 
 chinese$convert_minguo2western <- convert_minguo2western <- function(minguo_string){
   origin <- minguo_string
@@ -105,3 +115,5 @@ na_is_0 <- function(origin){
   origin[whichIsNA] = "0"
   return(origin)
 }
+
+
