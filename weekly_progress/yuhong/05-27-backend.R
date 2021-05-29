a87195b247c3d04097bd18103f1dd765a837a910
410777062 yuhong69 <su2000tww@gmail.com>
2021-05-29 14:02:21 +0800

    0529

--- weekly_progress/yuhong/05-27-backend.R
+++ weekly_progress/yuhong/05-27-backend.R
@@ -6,7 +6,7 @@
 browseURL("https://tpemartin.github.io/109-2-app101/checkin")
 # the frontend returns a numeric vector of GPS
 # c(latitude=..., longitude=...)
-returnedGPS = c(lon=24.94, lat=121.37)
+returnedGPS = c(lon=24.8, lat=121)
 heritage$validate_checkIn(returnedGPS) -> checkInResult
 # which check if the returnGPS is within 100 meters of any heritage site, 
 # If not, checkInResult = NULL;
@@ -21,7 +21,33 @@ if(!is.null(checkInResult))
   user$checkIns <- append(
     user$checkIns, list(checkInResult)
   )
+library(sp)
+spDistsN1(pts = ma, pt = ma2 , TRUE)
 
+
+
+matrix(
+  c(24.94,returnedGPS[1],121.37,returnedGPS[2]),
+  nrow=2
+) -> ma
+
+matrix(
+  c(10,120),
+  nrow=2
+) -> ma1
+
+matrix(
+  c(24.94,121.37),
+  nrow=2
+) -> ma2
+
+
+matrix(1:9, nrow = 3, ncol = 3)
+
+matrix()
+as.matrix(returnedGPS) -> matrix_gps2
+spDistsN1(pts = (24.94 : 121.37), pt = (24.95 , 122.38) , )
+spDistsN1(pts = matrix_gps1 , pt= (24,120), TRUE)
 
 # track -------------------------------------------------------------------
 # when user turn on tracking in the frontend, user's phone will track his GPX 
